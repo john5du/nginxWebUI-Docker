e78ac40ef7f7c1227e3c17e28cd4c2e8c300d4f2
DDSRem <1448139087@qq.com>
2023-03-06 15:34:45 +0800

    feat: use dumb-init

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ usermod -o -u "${PUID}" nginx
 chown -R ${PUID}:${PGID} /home/nginxWebUI.jar /home/nginxWebUI /var/lib/nginx /var/log/nginx /run/nginx
 
 cd /home
-exec su-exec ${PUID}:${PGID} java -jar -Dfile.encoding=UTF-8 -Xmx64m nginxWebUI.jar ${BOOT_OPTIONS} >/dev/null
+exec su-exec ${PUID}:${PGID} dumb-init java -jar -Dfile.encoding=UTF-8 -Xmx64m nginxWebUI.jar ${BOOT_OPTIONS} > /dev/null
